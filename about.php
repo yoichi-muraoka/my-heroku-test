02cb3325084313ad51b63ad765c9dea2a3614dbe
zdis56 <zdis56@sim.zdrv.com>
2020-12-03 16:00:08 +0900

    first commit

---
+++ about.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+	<meta charset="UTF-8">
+	</head>
+<body>
+<h1>Heroku Test</h1>
+<p>
+Heroku テスト
+</p>
+<a href="index.php">
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+	<meta charset="UTF-8">
+	</head>
+<body>
+<h1>Heroku Test</h1>
+<p>
+<?php echo "こんにちは"; ?>
+</p>
+<a href="about.php">
+</body>
+</html>
